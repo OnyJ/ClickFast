0ab7d3397675ce9f30cfe852885f338424a5eebe
Josselin <julien.josselin75@hotmail.fr>
2025-01-08 10:26:06 +0100

    Edit script: add handleGameButton() & startTimer()

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 const button = document.getElementById("button-clicker");
 const score = document.getElementById("score");
 let count = 0;
+let canPlay = true;
+let gameStarted = false;
+let timer = 5000;
 
-button.addEventListener("click", () => {
-  count++;
-  score.innerHTML = count;
-});
+function handleGameButton() {
+  button.addEventListener("click", () => {
+    if (!gameStarted) {
+      gameStarted = true;
+      startTimer();
+    }
+
+    if (canPlay) {
+      count++;
+    }
+    score.innerHTML = count;
+  });
+}
+
+function startTimer() {
+  const htmlTimer = document.getElementById("timer");
+  let timeLeft = 5;
+
+  for (let i = timeLeft; i >= 0; i--) {
+    setTimeout(() => {
+      htmlTimer.innerHTML = i;
+
+      if (i == 0) {
+        canPlay = false;
+      }
+    }, (timeLeft - i) * 1000);
+  }
+}
+
+handleGameButton();
